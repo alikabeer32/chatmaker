6ff04ac22e62562fd06b0d4fe63fcae79cf7d1c7
Ali Kabeer <kabeerali13@gmail.com>
2022-10-14 18:36:23 +0530

    new

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config()
 }
+require('dotenv').config()
 
-//require('dotenv').config()
 const url = require('url');
 const express = require('express');
 const bodyParser = require('body-parser');
